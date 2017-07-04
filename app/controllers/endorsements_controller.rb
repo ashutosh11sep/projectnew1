374ca22d8cb8702f7ebd01523fe54d9b2480badf
rakesh kumar <rakeshmaurya262@gmail.com>
2017-07-04 19:10:43 +0530

    endorse1

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -5,9 +5,10 @@ def create
     @endorsement = @post.endorsements.create(endorsement_params)
     @endorsement.user_id =current_user.id
     @endorsement.save
-    redirect_to post_path(@post)
+    redirect_to new_post_path
   end
- 
+
+  
   private
     def endorsement_params
       params.require(:endorsement).permit(:user_id, :text, :post_id)
