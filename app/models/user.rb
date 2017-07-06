ddedcecd7efbdc0d491aba18d55511c193138355
rakesh kumar <rakeshmaurya262@gmail.com>
2017-07-06 20:51:34 +0530

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,11 @@ has_many :taggings
 has_many :posts, through: :taggings
 
 
+has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
 
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
 
 def self.new_with_session(params, session)
   super.tap do |user|
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705095706) do
+ActiveRecord::Schema.define(version: 20170706150021) do
 
   create_table "comments", force: :cascade do |t|
     t.string "title", limit: 50, default: ""
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 20170705095706) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "post_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "following_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
+    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true
+    t.index ["following_id"], name: "index_follows_on_following_id"
   end
 
   create_table "messages", force: :cascade do |t|
